9a2619481695ffbbe7515d9bded805214779ebac
LINAGG1 <leeng060124@gmail.com>
2026-06-21 01:59:22 +0900

    fix: 진도율 반영

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import LearnPage from "./LearnPage";
-import QuizPage from "./QuizPage";
+import QuizPage from "./QuizPage"; // 퀴즈 페이지가 있다면
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/learn" element={<LearnPage />} />
+        
+        <Route path="/learn/:number" element={<LearnPage />} />
+        
         <Route path="/quiz" element={<QuizPage />} />
       </Routes>
     </BrowserRouter>
