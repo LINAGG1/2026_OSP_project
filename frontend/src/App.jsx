a0411a9c3bc61d179005b77f195e924abd4d2e09
LINAGG1 <leeng060124@gmail.com>
2026-06-21 00:57:57 +0900

    feat: Quiz 페이지 구현

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import LearnPage from "./LearnPage";
+import QuizPage from "./QuizPage";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/learn" element={<LearnPage />} />
+        <Route path="/quiz" element={<QuizPage />} />
       </Routes>
     </BrowserRouter>
   );
